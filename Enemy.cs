196697d0829a84f3684cc95f2f0c5f89cb4abc9a
karabay06 <46526531+karabay06@users.noreply.github.com>
2020-04-16 17:57:52 +0300

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Collider MainCollider;
+    public Collider[] AllCollider;
+    public GameObject Ok;
+    
+
+    private void Awake()
+    {
+        MainCollider = GetComponent<Collider>();
+        AllCollider = GetComponentsInChildren<Collider>(true);
+        DoRagdoll(false);
+        
+
+    }
+    private void Update()
+    {
+        //Ok.transform.LookAt(Main.transform);
+    }
+
+    public void DoRagdoll(bool isRagdoll)
+    {
+        foreach (var col in AllCollider)
+        {
+            col.enabled = isRagdoll;
+            MainCollider.enabled = !isRagdoll;
+            GetComponent<Rigidbody>().useGravity = !isRagdoll;
+            GetComponent<Animator>().enabled = !isRagdoll;
+        }
+    }
+}
